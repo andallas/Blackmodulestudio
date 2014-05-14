75d4365a593587e2a9102ac8384098ce641cc63d
Justin Hammond <justin.35f@gmail.com>
2014-05-14 11:42:47 -0700

    Cleaned up home page.

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -7,4 +7,54 @@
 		</center>
 	</div>
 
+	<div class="row">
+		<div class="col-xs-12 col-sm-6 col-md-4">
+			<div class="thumbnail">
+				<center><a href="<?php echo site_url('projects/katt') ?>"><img src="http://i1183.photobucket.com/albums/x480/andallas/KATT/katt_logo_zpscb0985e4.png"></a></center>
+				<div class="caption">
+					<h5><a href="<?php echo site_url('projects/katt') ?>">KATT</a></h5>
+					<h4>Release: 09/12</h4>
+					<a href="http://bit.ly/11XVAv6" class="btn btn-primary" role="button">Play</a>
+					<a href="<?php echo site_url('projects/katt-media') ?>" class="btn btn-primary" role="button">Media</a>
+					<a href="<?php echo site_url('projects/katt') ?>" class="btn btn-primary" role="button">Project</a>
+					<a href="#" class="btn btn-primary" role="button" disabled>Devlog</a>
+					<hr>
+					<p>KATT (Kill All The Things) is our first HTML5 game.</p>
+				</div>
+			</div>
+		</div>
+
+		<div class="col-xs-12 col-sm-6 col-md-4">
+			<div class="thumbnail">
+				<center><a href="<?php echo site_url('projects/tones-adventure') ?>"><img src="http://i1183.photobucket.com/albums/x480/andallas/Tones%20Adventure/Tone_AppLogo_zps4b04949e.png"></a></center>
+				<div class="caption">
+					<h5><a href="<?php echo site_url('projects/tones-adventure') ?>">Tone's Adventure</a></h5>
+					<h4>Release: 05/13</h4>
+					<a href="http://bit.ly/19hIRUo" class="btn btn-primary" role="button">Play</a>
+					<a href="<?php echo site_url('projects/tones-adventure-media') ?>" class="btn btn-primary" role="button">Media</a>
+					<a href="<?php echo site_url('projects/tones-adventure') ?>" class="btn btn-primary" role="button">Project</a>
+					<a href="#" class="btn btn-primary" role="button" disabled>Devlog</a>
+					<hr>
+					<p>Tone's Adventure is a small puzzle-platformer we made in Unity.</p>
+				</div>
+			</div>
+		</div>
+
+		<div class="col-xs-12 col-sm-6 col-md-4">
+			<div class="thumbnail">
+				<center><a href="<?php echo site_url('projects/lost-frontier') ?>"><img src="http://i1183.photobucket.com/albums/x480/andallas/Lost%20Frontier/Logo_zps0e8a4016.png"></a></center>
+				<div class="caption">
+					<h5><a href="<?php echo site_url('projects/lost-frontier') ?>">Lost Frontier</a></h5>
+					<h4>Release: TBD</h4>
+					<a href="#" class="btn btn-primary" role="button" disabled>Play</a>
+					<a href="<?php echo site_url('projects/lost-frontier-media') ?>" class="btn btn-primary" role="button">Media</a>
+					<a href="<?php echo site_url('projects/lost-frontier') ?>" class="btn btn-primary" role="button">Project</a>
+					<a href="<?php echo site_url('devlog/lost-frontier') ?>" class="btn btn-primary" role="button">Devlog</a>
+					<hr>
+					<p>Lost Frontier is our real-time, city-building, survival, exploration, tactical, strategy game that we are making in Unity.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+
 	<h3 class="text-muted">Latest News</h3>
